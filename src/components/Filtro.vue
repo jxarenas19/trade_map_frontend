f1bb95c7fd09ca73d4f9c87709bdb880185d81b6
julio Xavier <jxarenas@datamart.cl>
2023-02-19 23:10:45 -0500

    ppp

---
+++ src/components/Filtro.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="filter-container">
+        <el-input v-model="query.keyword" placeholder="nombre" style="width: 200px;" class="filter-item"
+                  @keyup.enter.native="handleFilter"/>
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+        </el-button>
+        <router-link :to="linkCrearNuevo">
+            <el-button type="primary" icon="el-icon-plus"
+                       v-permission="['gestionar nomencladores']" class="filter-item" style="margin-left: 10px;">
+                {{labelBotonCrearNuevo}}
+            </el-button>
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+  name: 'filtro',
+  props: {
+    handleFilter: {},
+    query: {},
+    linkCrearNuevo: {
+      type: String,
+      default: '/', // '/nomencladores/seriefacturacion/crear'
+    },
+    labelBotonCrearNuevo: {
+      type: String,
+      default: 'Nuevo',
+    },
+  },
+};
+</script>
